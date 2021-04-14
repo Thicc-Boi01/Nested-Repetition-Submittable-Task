43ff5dc3de504879bca26921377b863441280272
Thomas-Casey <102041956@student.swin.edu.au>
2021-04-14 21:58:22 +1000

    EX2

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NestedRepetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How Many X,es You Want?");
+            String Xno = Console.ReadLine();
+
+            int Xnoi;
+            Xnoi = Convert.ToInt32(Xno);
+
+            int counter1 = Xnoi;
+            int counter2 = 0;
+            int counter3 = 0;
+
+            while (counter1 > 0)
+            {
+
+                while (counter1 > 0)
+                {
+
+                    Console.Write("X");
+                    counter1--;
+                }
+                Console.WriteLine();
+                counter2++;
+                counter3++;
+                counter1 = Xnoi - counter3;
+            }
+            Console.WriteLine("How He Stand?");
+
+
+
+        }
+    }
+}
